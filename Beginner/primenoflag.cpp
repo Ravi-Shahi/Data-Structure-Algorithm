583e3a4b7e1c81cc17200e89158459ec97e35571
Ravindar Shahi <ravishahi1810@gmail.com>
2021-11-23 17:59:28 +0530

    Update Indentation

--- Beginner/primenoflag.cpp
+++ Beginner/primenoflag.cpp
@@ -1,21 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("../input.txt", "r", stdin);
-        freopen("../output.txt", "w", stdout);
-    #endif
-    int n,i;
-    cin>>n;
-    for(i=2;i<n;i++){
-        if(n%i==0){
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int n, i;
+    cin >> n;
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
             break;
         }
     }
-    if(i==n){
-        cout<<"Your number is PRIME";
-    }else{
-        cout<<"Not PRIME";
+    if (i == n)
+    {
+        cout << "Your number is PRIME";
+    }
+    else
+    {
+        cout << "Not PRIME";
     }
 }
